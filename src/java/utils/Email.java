0b397645093948c7316d05303fc41007cd87f84d
datnphe186023 <datnphe186023@fpt.edu.vn>
2024-02-27 19:24:53 +0700

    update email sender
    update charset encoding for vietnamese

--- src/java/utils/Email.java
+++ src/java/utils/Email.java
@@ -36,51 +36,10 @@ public class Email {
             message.addHeader("Content-type", "text/HTML; charset=UTF-8");
             message.setFrom(from);
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
-            message.setSubject(subject);
+            message.setSubject(subject, "UTF-8");
             message.setSentDate(new Date());
             //message.setReplyTo(InternetAddress.parse(from, false));
             message.setText(content, "UTF-8");
-
-            Transport.send(message);
-
-        } catch (MessagingException e) {
-            System.out.println("SendEmail " + e);
-        }
-    }
-
-    public static void main(String[] args) {
-        final String from = "datnguyenphuong1810@gmail.com";
-        final String password = "gont wetg ouly xnqc";
-
-        // Properties :
-        Properties properties = new Properties();
-        properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", "587");
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-
-        // create Authenticator
-        Authenticator authenticator = new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(from, password);
-            }
-        };
-
-        //Phien lam viec
-        Session session = Session.getInstance(properties, authenticator);
-
-        //Sending email
-        MimeMessage message = new MimeMessage(session);
-
-        try {
-            message.addHeader("Content-type", "text/HTML; charset=UTF-8");
-            message.setFrom(from);
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("datnphe186023@fpt.edu.vn", false));
-            message.setSubject("This is it");
-            message.setSentDate(new Date());
-            //message.setReplyTo(InternetAddress.parse(from, false));
-            message.setText("body heere", "UTF-8");
 
             Transport.send(message);
 
